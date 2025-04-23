f76ddbdaafc0c531cef2c446fab54e762ca9e0f4
vaqsi1990 <vaqsii23@gmail.com>
2025-04-23 17:59:09 +0400

    Removed unwanted .rar file

--- components/switcher.tsx
+++ components/switcher.tsx
@@ -26,8 +26,8 @@ export default function LocaleSwitcher() {
   };
 
   const locales = [
-    { code: 'ge', label: 'ქართული', flag: Ge },
     { code: 'en', label: 'English', flag: Am },
+    { code: 'ge', label: 'ქართული', flag: Ge },
   ];
 
   const currentLocale = locales.find(l => l.code === localeActive);
