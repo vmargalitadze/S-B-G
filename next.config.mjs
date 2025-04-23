20a30a1fc69860f9c89c9fd4992de41ec7fee0bf
vaqsii <vaqsii23@gmail.com>
2025-04-23 18:52:55 +0400

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: true,
+  
     images: {
       remotePatterns: [
         {
