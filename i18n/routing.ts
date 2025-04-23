aa58e8a0841f43ddb8fbffd899d6cdd47e26b7e6
vaqsi1990 <vaqsii23@gmail.com>
2025-04-23 17:02:51 +0400

    Removed unwanted .rar file

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,7 +3,7 @@ import {createNavigation} from 'next-intl/navigation';
  
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['en', 'ge'],
+  locales: ['ge', 'en'],
  
   // Used when no locale matches
   defaultLocale: 'ge'
