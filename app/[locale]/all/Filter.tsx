002142bdb50ff806cccf43f79e9d938c8a923220
vaqsii <vaqsii23@gmail.com>
2025-04-28 01:03:39 +0400

    active button changes improve

--- app/[locale]/all/Filter.tsx
+++ app/[locale]/all/Filter.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { ProductType } from "@prisma/client";
 import React from "react";
-import { useTranslations } from "next-intl"; // ქართული და ინგლისური ტექსტისთვის
+import { useTranslations } from "next-intl"; 
 
 function Filter({ selectedCategory, setSelectedCategory }: FilterProps) {
-  const t = useTranslations("products"); // თარგმანის ჰუკი
+  const t = useTranslations("products"); 
   const categories: ProductType[] = ["MATTRESS", "PILLOW", "QUILT", "PAD"];
   
   const handleCategoryChange = (category: ProductType) => {
@@ -12,9 +12,8 @@ function Filter({ selectedCategory, setSelectedCategory }: FilterProps) {
   };
 
   const handleReset = () => {
-    setSelectedCategory(undefined); // Reset to show all products
+    setSelectedCategory(undefined); 
   };
-console.log(categories);
 
   return (
     <div className="pt-[70px] pb-[70px]">
@@ -26,31 +25,27 @@ console.log(categories);
             className={`outline-none rounded-lg cursor-pointer transition-all duration-150 ease-in-out 
             border border-solid border-black px-4 py-2 sm:px-6 sm:py-3 
             text-[14px] sm:text-[18px] uppercase focus:outline-none 
-            hover: bg-[#203e72] hover:text-white
-            ${selectedCategory === undefined ? " bg-[#203e72] text-white" : "text-black"}`}
+            hover:bg-[#203e72] hover:text-white
+            ${selectedCategory === undefined ? " bg-[#203e72] text-white" : "text-black"}`}  
             onClick={handleReset}
           >
-            {t("all")} {/* Translate "All" to Georgian or English */}
+            {t("all")} 
           </button>
 
           {/* Categories */}
           {categories.map((category, i) => (
             <div key={i}>
-           
-            
-            <button
-              key={category}
-              onClick={() => handleCategoryChange(category)}
-              className={`
-                outline-none rounded-lg cursor-pointer transition-all duration-150 ease-in-out 
+              <button
+                onClick={() => handleCategoryChange(category)}
+                className={`outline-none rounded-lg cursor-pointer transition-all duration-150 ease-in-out 
                 border border-solid border-black px-4 py-2 sm:px-6 sm:py-3 
                 text-[14px] sm:text-[18px] uppercase focus:outline-none 
                 hover:bg-[#203e72] hover:text-white
                 ${selectedCategory === category ? " bg-[#203e72] text-white" : "text-black"}
               `}
-            >
-              {t(category)} {/* Translate category text */}
-            </button>
+              >
+                {t(category)} {/* Translate category text */}
+              </button>
             </div>
           ))}
 
